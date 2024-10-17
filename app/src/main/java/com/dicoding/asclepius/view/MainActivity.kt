f48cbee458235c3baaa671580137112d0758c77a
Gorthdzx <krislaksana123@gmail.com>
2024-10-17 11:24:49 +0700

    feat : image classification using tflite

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -2,41 +2,132 @@ package com.dicoding.asclepius.view
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import com.dicoding.asclepius.R
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.dicoding.asclepius.databinding.ActivityMainBinding
+import com.dicoding.asclepius.helper.ImageClassifierHelper
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private var currentImageUri: Uri? = null
+    private var results: String = " "
+    private var confidenceScore: String = ""
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { it: Boolean ->
+            if (it) {
+                showToast("Permission request granted")
+            } else {
+                showToast("Permission request denied")
+            }
+        }
+
+
+    private fun allPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        REQUIRED_PERMISSION
+    ) == PackageManager.PERMISSION_GRANTED
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!allPermissionGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.analyzeButton.setOnClickListener {
+            analyzeImage()
+            moveToResult()
+        }
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) {
+        if (it != null) {
+            currentImageUri = it
+            showImage()
+        } else {
+            Log.d("PHOTO PICKER", "NO MEDIA SELECTED")
+        }
     }
 
     private fun startGallery() {
         // TODO: Mendapatkan gambar dari Gallery.
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
 
     private fun showImage() {
         // TODO: Menampilkan gambar sesuai Gallery yang dipilih.
+        currentImageUri?.let {
+            Log.d("IMAGE URI ", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
     }
 
     private fun analyzeImage() {
         // TODO: Menganalisa gambar yang berhasil ditampilkan.
+        currentImageUri?.let {
+            val imageClassifierHelper = ImageClassifierHelper(
+                context = this,
+                classifierListener = object : ImageClassifierHelper.ClassifierListener {
+                    override fun error(error: String) {
+                        Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onResults(result: List<Classifications>?, inferenceTime: Long) {
+                        result?.let { classif ->
+                            if (classif.isNotEmpty() && classif[0].categories.isNotEmpty()) {
+                                println(classif)
+                                val sortedCategory =
+                                    classif[0].categories.sortedByDescending { score ->
+                                        score?.score
+                                    }
+                                results = sortedCategory.joinToString("\n") { res ->
+                                    "${res.label} " + NumberFormat.getPercentInstance()
+                                        .format(res.score).trim()
+                                }
+                                Log.d("RESULTCLAS", results)
+                                confidenceScore = NumberFormat
+                                    .getPercentInstance()
+                                    .format(sortedCategory[0].score)
+                                Log.d("SCORECCONFIDENCE", confidenceScore)
+                            }
+                        }
+                    }
+
+                }
+            )
+            imageClassifierHelper.classifyStaticImage(it)
+        } ?: showToast("No Image Selected")
     }
 
     private fun moveToResult() {
         val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra(ResultActivity.EXTRA_IMAGE_URI, currentImageUri.toString())
+        intent.putExtra(ResultActivity.EXTRA_RESULT, results.lines().firstOrNull())
+        intent.putExtra(ResultActivity.EXTRA_SCORE, confidenceScore)
         startActivity(intent)
     }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 }
